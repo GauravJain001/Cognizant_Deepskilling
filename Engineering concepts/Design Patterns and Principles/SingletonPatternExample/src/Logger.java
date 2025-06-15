2b0b4ba14c39f82f9a0ae922d986752ab3538aff
Gaurav Jain <gauravjain.0184@gmail.com>
2025-06-15 18:55:06 +0530

    single complete

---
+++ Engineering concepts/Design Patterns and Principles/SingletonPatternExample/src/Logger.java
@@ -0,0 +1,16 @@
+public class Logger {
+    private static Logger instance = null;
+    private Logger(){
+        System.out.println("Logger Constructor Called");
+    }
+    public static Logger getInstance(){
+        if(instance == null){
+            instance = new Logger();
+        }
+        return instance;
+    }
+    public void log(String message){
+        System.out.println("Log: "+message);
+    }
+
+}
