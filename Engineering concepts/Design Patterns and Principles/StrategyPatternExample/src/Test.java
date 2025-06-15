ba3a55c5e227df9804790960b464be043c184a18
Gaurav Jain <gauravjain.0184@gmail.com>
2025-06-15 15:27:39 +0530

    strategy design pattern complete

---
+++ Engineering concepts/Design Patterns and Principles/StrategyPatternExample/src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String [] args){
+        // Initilizing strategies
+        PaymentStrategy CreditCardPaymentStrategy = new CreditCardPayment();
+        PaymentStrategy PayPalPaymentStrategy = new PayPalPayment();
+
+        // initilazing the client with strategy-credit card
+        PaymentContext paymentContext = new PaymentContext(CreditCardPaymentStrategy);
+        paymentContext.pay(100);
+
+        //initilizing the client with strategy with paypal
+        paymentContext = new PaymentContext(PayPalPaymentStrategy);
+        paymentContext.pay(10);
+        
+    }
+}
