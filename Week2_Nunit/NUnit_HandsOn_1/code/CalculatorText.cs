d494343f3dbd167ca096a327ae89b9a8a990af51
Gaurav Jain <gauravjain.0184@gmail.com>
2025-06-29 11:31:27 +0530

    Add unit tests for SimpleCalculator operations and project configuration

---
+++ Week2_Nunit/NUnit_HandsOn_1/code/CalculatorText.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using CalcLibrary;
+using NUnit.Framework;
+namespace code
+{
+    public class CalculatorTest
+    {
+        private SimpleCalculator calculator;
+        [SetUp]
+        public void Init()
+        {
+            calculator = new SimpleCalculator();
+        }
+        [TearDown]
+        public void Cleanup()
+        {
+            calculator.AllClear();
+        }
+        [TestCase(10,5,15)]
+        [TestCase(-2, 3, 1)]
+        [TestCase(0, 0, 0)]
+        public void TestAddition(double a, double b, double expected)
+        {
+            double actual = calculator.Addition(a, b);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        [TestCase(15,5,10)]
+        [TestCase(10,2,8)]
+        [TestCase(8,8,0)]
+        public void TestSubtraction(double a, double b, double expected)
+        {
+            double result = calculator.Subtraction(a, b);
+            Assert.That(result,Is.EqualTo(expected));
+        }
+        [TestCase(2, 2, 4)]
+        [TestCase(10, 0, 0)]
+        [TestCase(8, 8, 64)]
+        public void TestMultiplication(double a, double b, double expected)
+        {
+            double result = calculator.Multiplication(a, b);
+            Assert.That(result,Is.EqualTo(expected));
+        }
+        //[TestCase(2, 2, 1)]
+        //[TestCase(10, 5, 2)]
+        //[TestCase(8, 4, 2)]
+        //public void TestDivision(double a, double b, double expected)
+        //{
+        //    double result = calculator.Division(a, b);
+        //    Assert.That(result, Is.EqualTo(expected));
+        //}
+    }
+}
---
+++ Week2_Nunit/NUnit_HandsOn_2/code/CalculatorTest.cs
@@ -0,0 +1,69 @@
+using System;
+using CalcLibrary;
+namespace code
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        public SimpleCalculator Calculator { get; set; }
+
+        [SetUp]
+        public void Init()
+        {
+            Calculator = new SimpleCalculator();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            Calculator.AllClear();
+        }
+        [TestCase(5,5,0)]
+        [TestCase(5,4,1)]
+        [TestCase(9,5,4)]
+        public void TestSubtraction(double a,double b,double expected)
+        {
+            double result = Calculator.Subtraction(a,b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        [TestCase(5, 5, 25)]
+        [TestCase(5, 4, 20)]
+        [TestCase(9, 4, 36)]
+        public void TestMultiplication(double a, double b, double expected) 
+        { 
+            double result = Calculator.Multiplication(a,b);
+            Assert.That(result, Is.EqualTo(expected));
+
+        }
+        [TestCase(5, 5, 1)]
+        [TestCase(5, 2, 2.5)]
+        [TestCase(9, 0, 3)]
+        public void TestDivision(double a, double b, double expected)
+        {
+            try
+            {
+                double result = Calculator.Division(a, b);
+                Assert.That(result, Is.EqualTo(expected));
+            }
+            catch(ArgumentException ex)
+            {
+                Assert.That(ex.Message, Is.EqualTo("Second Parameter Can't be Zero"));
+                Assert.Fail("Division By Zero");
+            }
+            
+        }
+        [Test]
+        public void TestAddAndClear()
+        {
+            Calculator.Addition(5, 5);
+            Assert.That(10, Is.EqualTo(Calculator.GetResult));
+            Calculator.AllClear();
+            Assert.That(0, Is.EqualTo(Calculator.GetResult));
+
+        }
+
+
+
+
+
+    }
+}
