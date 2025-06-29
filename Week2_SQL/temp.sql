8c01909135b2f3b054a2441897e7c057ec400f3c
Gaurav Jain <gauravjain.0184@gmail.com>
2025-06-29 19:35:07 +0530

    Remove unused SQL function for annual salary calculation and related query

--- Week2_SQL/temp.sql
+++
@@ -1,6 +0,0 @@
-CREATE FUNCTION fn_CalculateAnnualSalary(@Salary DECIMAL(10,2))
-RETURNS DECIMAL(10,2) AS BEGIN RETURN @Salary * 12;
-END;
-
-SELECT EmployeeID, FirstName, LastName, Salary, dbo.fn_CalculateAnnualSalary(Salary) AS AnnualSalary
-FROM Employees;
