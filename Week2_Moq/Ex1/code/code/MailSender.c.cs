401065d8a434aa06890d4d081b97155e24eb48c3
Gaurav Jain <gauravjain.0184@gmail.com>
2025-06-29 21:31:46 +0530

    Add CustomerComm and IMailSender classes with unit tests for email functionality

---
+++ Week2_Moq/Ex1/code/code/MailSender.c.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+
+namespace code
+{
+    public class MailSender : IMailSender
+    {
+        public bool SendMail(string toAddress, string message)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("your_email_address@gmail.com");
+                mail.To.Add(toAddress);
+                mail.Subject = "Test Mail";
+                mail.Body = message;
+
+                smtpServer.Port = 587;
+                smtpServer.Credentials = new NetworkCredential("username", "password");
+                smtpServer.EnableSsl = true;
+
+                smtpServer.Send(mail);
+                return true;
+            }
+            catch
+            {
+                // If any error occurs while sending mail
+                return false;
+            }
+        }
+    }
+}
